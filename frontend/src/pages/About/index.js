804dff0609382d7f5ce666650db082084114fef9
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2025-01-18 22:31:13 +0100

    about theme

--- frontend/src/pages/About/index.js
+++ frontend/src/pages/About/index.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import { Divider } from "antd";
 
 import TravelIMG from "../../assets/images/travel.jpg";
@@ -5,13 +6,26 @@ import MissionIMG from "../../assets/images/mission.jpg";
 import PromiseIMG from "../../assets/images/promise.jpg";
 
 export default function About() {
+    const themeMode = useSelector((state) => state.theme.themeMode);
     return (
         <>
-            <Divider orientation="left">
-                <h1 style={{textTransform: "uppercase", fontWeight: "800",fontSize: "25px", margin: 0}}>About us</h1>
+            <Divider orientation="left" style={{borderColor: (themeMode === "dark" ? "rgba(255,255,255,0.20)" : "rgba(5,5,5,0.06)")}}>
+                <h1 style={{
+                    textTransform: "uppercase",
+                    fontWeight: "800",
+                    fontSize: "25px",
+                    margin: 0,
+                    color: (themeMode === "dark" ? "#FFFFFF" : "#000000")}}
+                >
+                    About us
+                </h1>
             </Divider>
             <div style={{width:"50%"}}>
-                <p>
+                <p style={{
+                    color: (themeMode === "dark" ? "#FFFFFF" : "#000000"),
+                    fontSize: "20px",
+                    fontWeight: 400}}
+                >
                     Welcome to Plan&Stay, your trusted partner in travel planning and accommodation booking.<br/>
 
                     At Plan&Stay, we believe that every journey begins with the perfect stay. Whether you’re embarking
@@ -30,17 +44,38 @@ export default function About() {
             <div style={{display: "flex", justifyContent: "space-between", marginTop: "50px"}}>
                 <div style={{display: "flex", flexDirection: "column", gap: "50px", width: "50%"}}>
                     <div style={{display: "flex", flexDirection: "column"}}>
-                        <h1 style={{textTransform: "uppercase", fontWeight: "800", margin: 0}}>Our Mission</h1>
-                        <p>
+                        <h1 style={{
+                            textTransform: "uppercase",
+                            fontWeight: "800",
+                            margin: 0,
+                            color: (themeMode === "dark" ? "#FFFFFF" : "#000000")}}
+                        >
+                            Our Mission
+                        </h1>
+                        <p style={{
+                            color: (themeMode === "dark" ? "#FFFFFF" : "#000000"),
+                            fontSize: "20px",
+                            fontWeight: 400}}
+                        >
                             To empower travelers by offering an easy, transparent, and stress-free booking experience
-                            that
-                            turns travel dreams into reality.
+                            that turns travel dreams into reality.
                         </p>
                     </div>
 
                     <div style={{display: "flex", flexDirection: "column"}}>
-                        <h1 style={{textTransform: "uppercase", fontWeight: "800", margin: 0}}>Why Choose Us?</h1>
-                        <p>
+                        <h1 style={{
+                            textTransform: "uppercase",
+                            fontWeight: "800",
+                            margin: 0,
+                            color: (themeMode === "dark" ? "#FFFFFF" : "#000000")}}
+                        >
+                            Why Choose Us?
+                        </h1>
+                        <p style={{
+                            color: (themeMode === "dark" ? "#FFFFFF" : "#000000"),
+                            fontSize: "20px",
+                            fontWeight: 400}}
+                        >
                             Intuitive Experience: With our user-friendly platform, booking is as easy as a few clicks.
                             Personalized Options: Our smart recommendations help you find the perfect place to stay
                             based on your preferences.
@@ -64,8 +99,19 @@ export default function About() {
                 </div>
 
                 <div style={{display: "flex", flexDirection: "column", width: "50%"}}>
-                    <h1 style={{textTransform: "uppercase", fontWeight: "800", margin: 0}}>Our Promise</h1>
-                    <p>
+                    <h1 style={{
+                        textTransform: "uppercase",
+                        fontWeight: "800",
+                        margin: 0,
+                        color: (themeMode === "dark" ? "#FFFFFF" : "#000000")}}
+                    >
+                        Our Promise
+                    </h1>
+                    <p style={{
+                        color: (themeMode === "dark" ? "#FFFFFF" : "#000000"),
+                        fontSize: "20px",
+                        fontWeight: 400}}
+                    >
                         At Plan&Stay, we’re more than just a booking platform. We’re passionate about creating memorable
                         travel
                         experiences and helping you find comfort wherever your journey takes you.<br/>
--- frontend/src/pages/Hotels/index.js
+++ frontend/src/pages/Hotels/index.js
@@ -1,14 +1,31 @@
+import { useSelector } from "react-redux";
+
 import HotelsItem from "./HotelsItem";
 
 import { Divider } from "antd";
 
 export default function Hotels() {
+    const themeMode = useSelector((state) => state.theme.themeMode);
+
     return (
         <>
-            <Divider orientation="left">
-                <h1 style={{textTransform: "uppercase", fontWeight: "800", fontSize: "25px", margin: 0}}>All Hotels</h1>
+            <Divider orientation="left" style={{borderColor: (themeMode === "dark" ? "rgba(255,255,255,0.20)" : "rgba(5,5,5,0.06)")}}>
+                <h1 style={{
+                    textTransform: "uppercase",
+                    fontWeight: "800",
+                    fontSize: "25px",
+                    margin: 0,
+                    color: (themeMode === "dark" ? "#FFFFFF" : "#000000")}}
+                >
+                    All Hotels
+                </h1>
             </Divider>
-            <h4 style={{marginLeft: "4%", color: "rgba(0,0,0,0.50)"}}>Choose the best hotel for yourself</h4>
+            <h4 style={{
+                marginLeft: "4%",
+                color: (themeMode === "dark" ? "rgba(255,255,255,0.5)" : "rgba(0,0,0,0.50)")}}
+            >
+                Choose the best hotel for yourself
+            </h4>
             <HotelsItem/>
         </>
     )
