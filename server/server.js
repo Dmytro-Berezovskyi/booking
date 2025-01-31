eec514d218a9f602694096c3c0d9f298427785f8
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2025-01-31 11:46:07 +0100

    1

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const data = require("./db.json");
+const { json } = require("express");
 
 const app = express();
 app.use(cors());
